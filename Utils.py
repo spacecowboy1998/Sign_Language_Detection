99d1c52812f4fec9d08febd3e83ca925490b5cea
akakibenidze <akaki.benidze.1@btu.edu.ge>
2024-11-24 15:04:35 +0400

    Utilities to help Analyzing images

---
+++ Utils.py
@@ -0,0 +1,224 @@
+import copy
+import itertools
+import csv
+import cv2
+import numpy as np
+
+
+def calc_landmark_list(image, landmarks):
+    """
+     Calculate landmark points based on the given image and landmarks.
+
+     Args:
+         image: The input image.
+         landmarks: Landmarks object from Mediapipe, containing x, y attributes.
+
+     Returns:
+         List of landmark points as (x, y) coordinates relative to the input image size.
+    """
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+def pre_process_landmark(landmark_list):
+    """
+    Preprocess the list of landmark points by normalizing and converting them to relative coordinates.
+
+    Args:
+        landmark_list: List of landmark points as (x, y) coordinates.
+
+    Returns:
+        A normalized, flattened, one-dimensional list of landmarks relative to the first point.
+    """
+
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    base_x, base_y = temp_landmark_list[0]
+    for index, landmark_point in enumerate(temp_landmark_list):
+        temp_landmark_list[index][0] -= base_x
+        temp_landmark_list[index][1] -= base_y
+
+    temp_landmark_list = list(itertools.chain.from_iterable(temp_landmark_list))
+
+    max_value = max(map(abs, temp_landmark_list)) if temp_landmark_list else 1
+
+    temp_landmark_list = [n / max_value for n in temp_landmark_list]
+
+    return temp_landmark_list
+
+def draw_landmarks(image, landmark_point, brect=None):
+    """
+    Draw the landmarks and their connections on the image for visualization.
+
+    Args:
+        image: The image on which the landmarks will be drawn.
+        landmark_point: List of (x, y) coordinates of the landmarks.
+        brect: Optional bounding rectangle for the hand (default is None).
+
+    Returns:
+        The image with drawn landmarks and optional bounding rectangle.
+    """
+    if len(landmark_point) > 0:
+        # Thumb
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
+                 (255, 255, 255), 2)
+
+        # Index finger
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
+                 (255, 255, 255), 2)
+
+        # Middle finger
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
+                 (255, 255, 255), 2)
+
+        # Ring finger
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
+                 (255, 255, 255), 2)
+
+        # Little finger
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
+                 (255, 255, 255), 2)
+
+        # Palm
+        cv2.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
+                 (255, 255, 255), 2)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
+                 (0, 0, 0), 6)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
+                 (255, 255, 255), 2)
+
+    if brect:
+        cv2.rectangle(image, (brect[0] - 20, brect[1] - 20), (brect[2] + 20, brect[3] + 20),
+                  (51, 204, 255), 3)
+
+    return image
+
+def calc_bounding_rect(image, landmarks):
+    """
+    Calculate the bounding rectangle for the given landmarks.
+
+    Args:
+        image: The input image in which the bounding rectangle is to be calculated.
+        landmarks: Landmarks object, containing x, y attributes for hand landmarks.
+
+    Returns:
+        A list containing the coordinates of the bounding rectangle: [x1, y1, x2, y2].
+    """
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv2.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+def logging_landmarks(class_id, landmark_list):
+    """
+    Log the landmark data for a specific class into a CSV file.
+
+    Args:
+        class_id: The class identifier (integer) representing the gesture class id.
+        landmark_list: List of normalized landmark points to log.
+    """
+    path = 'Model/hand_gesture/hand_gesture_landmarks.csv'
+    with open(path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([class_id] + landmark_list)
+
+def logging_name(class_names):
+    """
+    Log the class names into a CSV file.
+
+    Args:
+        class_names: List of class names representing different gesture types.
+    """
+
+    path = 'Model/hand_gesture/hand_gesture_label.csv'
+    with open(path, 'w', newline="", encoding='utf-8') as f:
+        writer = csv.writer(f)
+        for class_id, class_name in enumerate(class_names):
+            writer.writerow([class_name])
